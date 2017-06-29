1f938d4d493658ae7043778c2d43ce9867ef34f1
gunhee cho <ckhee93@gmail.com>
2017-06-29 10:08:43 +0900

    2579 solved

---
+++ DP1/2579-upstair.cpp
@@ -0,0 +1,26 @@
+#include <cstdio>
+#include <algorithm>
+using namespace std;
+
+int main()
+{
+    int N = 0;
+    int d[301][2];
+    int step[301];
+    scanf("%d", &N);
+
+    for (int i = 0; i < N; i++)
+    {
+        scanf("%d", &step[i]);
+    }
+
+    d[0][0] = 0;
+    d[0][1] = step[0];
+    for (int i = 1; i < N; i++)
+    {
+        d[i][0] = d[i-1][1]+step[i];
+        d[i][1] = max(d[i-2][0],d[i-2][1])+step[i];
+    }
+
+    printf("%d\n",max(d[N-1][0],d[N-1][1]));
+}
