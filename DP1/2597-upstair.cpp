f5363361ddf920b3142bd209d0101ba25cc26b93
gunhee cho <ckhee93@gmail.com>
2017-07-02 16:37:47 +0900

    modify upstair

--- DP1/2597-upstair.cpp
+++ DP1/2597-upstair.cpp
@@ -14,12 +14,14 @@ int main()
         scanf("%d", &step[i]);
     }
 
-    d[0][0] = 0;
+    d[0][0] = step[0];
     d[0][1] = step[0];
+    d[1][1] = step[1];
     for (int i = 1; i < N; i++)
     {
         d[i][0] = d[i-1][1]+step[i];
-        d[i][1] = max(d[i-2][0],d[i-2][1])+step[i];
+        if(i >= 2)
+            d[i][1] = max(d[i-2][0],d[i-2][1])+step[i];
     }
 
     printf("%d\n",max(d[N-1][0],d[N-1][1]));
