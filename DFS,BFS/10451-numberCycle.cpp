da4bf5107cd1673747128583c853b091e9613492
gunhee cho <ckhee93@gmail.com>
2017-07-10 23:17:55 +0900

    10451 순열 사이클

---
+++ DFS,BFS/10451-numberCycle.cpp
@@ -0,0 +1,34 @@
+#include <cstdio>
+using namespace std;
+
+int main(){
+    int T,N;
+    int arr[1000];
+    int check[1000] = {};
+    int j = 0,res = 0;
+    scanf("%d",&T);
+
+    while(T > 0){
+        res = 0;
+        j = 0;
+        scanf("%d",&N);
+        for(int i = 1;i<=N;i++){
+            scanf("%d",&arr[i]);
+            check[i] = 0;
+        }
+        for(int i = 1;i<=N;i++){
+            if(check[i] != 1){
+                j = arr[i];
+                while(j != i){
+                    check[j] = 1;
+                    //printf("ss%d\n",j);
+                    j = arr[j];
+                }
+                //printf("dd%d\n",i);
+                res++;
+            }
+        }
+        printf("%d\n",res);
+        T--;
+    }
+}
