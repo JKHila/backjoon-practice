1eb3905f9455a0d9b3fdb75512fbdf92ce5bd067
gunhee cho <ckhee93@gmail.com>
2017-07-18 15:14:54 +0900

    11048 이동하기

--- DP1/11048 - move.cpp
+++ DP1/11048 - move.cpp
@@ -1,36 +1,27 @@
 #include <cstdio>
+#include <algorithm>
 using namespace std;
 
-int dp[1001][1001];
-int c[1001][1001];
-
-
-int candy(int x,int y){
-    if(dp[x][y] != -1){
-        return dp[x][y];
-    }
-
-    dp[x][y] += candy(x-1,y) + candy(x,y-1) + candy(x-1,y-1);
-    return c[x][y];
-}
 
 int main(){
     int N,M;
+    int dp[1001][1001] = {};
+    int c[1001][1001];
     scanf("%d %d",&N,&M);
 
     for(int i = 1;i<=N;i++){
         for(int j = 1;j<=M;j++){
-            dp[i][j] = -1;
             scanf("%d",&c[i][j]);
+            dp[i][j] = c[i][j] + max(dp[i-1][j],max( dp[i][j-1] , dp[i-1][j-1]));
         }    
     }
     
     printf("%d\n",dp[N][M]);
 
-    for(int i = 1;i<=N;i++){
+    /*for(int i = 1;i<=N;i++){
         for(int j = 1;j<=M;j++){
             printf("%d ",dp[i][j]);
         }
     printf("\n");
-    }
+    }*/
 }
