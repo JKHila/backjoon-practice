99e167858656a8b668b48e9eb38081e1e09999a3
jkhila <ckhee93@gmail.com>
2017-07-28 11:51:35 +0900

    11727 2*n 타일링2

---
+++ DP1/11727 - 2*nTileing2.cpp
@@ -0,0 +1,18 @@
+#include <cstdio>
+using namespace std;
+
+int main(){
+    int N;
+    int dp[1001]={};\
+    int mod = 10007;
+    scanf("%d",&N);
+
+    dp[0] = 1;
+    dp[1] = 1;
+
+    for(int i = 2;i<=N;i++){
+        dp[i] = dp[i-1] + dp[i-2]*2;
+        dp[i] %= mod;
+    }
+    printf("%d\n",dp[N]%mod);
+}
