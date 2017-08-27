a4123110fda7ee4bd0771f12340637b4998581be
gunhee cho <ckhee93@gmail.com>
2017-08-28 00:36:14 +0900

    9663 N-Queen

--- BackTracking/9663 - N-Queen.cpp
+++ BackTracking/9663 - N-Queen.cpp
@@ -1,10 +1,9 @@
 #include <cstdio>
-#include <vector>
 #include <cmath>
 using namespace std;
 int N;
 int res = 0;
-bool check(vector<int> vec, int newRow){
+bool check(int vec[], int newRow){
     int curRow = 0;
     while(curRow < newRow){
         if(vec[newRow] == vec[curRow] || fabs(vec[newRow]-vec[curRow])==(newRow-curRow)){
@@ -14,10 +13,10 @@ bool check(vector<int> vec, int newRow){
     }
     return false;
 }
-void f(vector<int> vec,int row){
+void f(int vec[] ,int row){
     if(!check(vec,row)){
         if(row == N-1){                
-            res++;
+            ++res;
         }else{
             for(int i = 0;i<N;++i){
                 vec[row+1] = i;
@@ -29,10 +28,10 @@ void f(vector<int> vec,int row){
 int main(){
     scanf("%d",&N);
 
-    vector<int> vec(N,0);
+    int col[15];
     for(int i = 0;i<N;++i){
-        vec[0] = i;
-        f(vec,0);
+        col[0] = i;
+        f(col,0);
     }
     printf("%d\n",res);
 }
