4ec12fecf5a336b0c2cee79f0fe346ff17af6a24
Gunhee Jo <ckhee93@gmail.com>
2017-08-31 19:07:40 +0900

    2941 안됨..

--- a.py
+++ a.py
@@ -2,11 +2,15 @@ n = ["c=","c-","dz=","d-","lj","nj","s=","z="]
 st = input()
 
 res = 0
-
-for key in n:
-    if st.find(key) != -1:
-        print(key)
-        st = st.replace(key,"")
-        res += 1
+cnt = 0
+while cnt < 8:
+    for key in n:
+        idx = st.find(key)
+        if idx != -1:
+            st = st.replace(key,"",1)
+            res += 1
+            cnt = 0
+            break
+        cnt += 1
 res += len(st)
 print(res)
