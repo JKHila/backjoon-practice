1d393854d226ae2ee1ddca981fb5d95fa95af699
jkhila <ckhee93@gmail.com>
2017-11-15 19:12:29 +0900

    그리디 알고리즘 과제 - 냉장고

---
+++ Greedy/19-refrigerator.cpp
@@ -0,0 +1,31 @@
+#include <cstdio>
+#include <vector>
+#include <algorithm>
+using namespace std;
+int main(){
+    int N,a,b;
+    vector<pair<int, int> > arr;
+    scanf("%d",&N);
+    for(int i = 0;i<N;i++){
+        scanf("%d %d",&a,&b);
+        arr.push_back(make_pair(a,b));
+    }
+    sort(arr.begin(),arr.end());
+ 
+    int res = 1;
+    int refNum = 0;
+    for(int i = 1;i<N;i++){
+        bool flag = false;
+        for(int j = refNum;j<i;j++){
+            if(arr[i].first > arr[j].second){
+                flag = true;
+                refNum = i;
+                break;
+            }
+        }
+        if(flag)
+            res++;
+    }
+    printf("%d\n",res);
+     
+}
